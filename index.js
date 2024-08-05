853e15bd5e2476938f1d6c4127603968301363fb
ValdoYKING <osvaldovillalba-02@hotmail.com>
2024-08-05 14:23:45 -0600

    Conexion con mongo DB y se define patron MVC

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+const dotenv = require('dotenv');
+const connectDB = require('./src/config/db');
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+// Configurar EJS como motor de plantillas
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'src/views'));
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/api', require('./src/routes'));
+//./src/routes
+// Ruta para renderizar una vista de ejemplo con EJS
+app.get('/', (req, res) => {
+  res.render('index', { title: 'Mi aplicación Express' });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
