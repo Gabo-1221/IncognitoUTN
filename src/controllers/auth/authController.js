dd8f63c53b231d9988495ef0eeeb96ae03f724f4
Osvaldo Villalba Mendoza <137123804+ValdoYKING@users.noreply.github.com>
2024-08-08 14:53:32 -0600

    envio de datos

--- src/controllers/auth/authController.js
+++ src/controllers/auth/authController.js
@@ -1,22 +1,29 @@
-// controllers/auth/authController.js
+// src/controllers/auth/authController.js
+
+// Muestra el formulario de inicio de sesión
+exports.formLogin = (req, res) => {
+  res.render('auth/formLogin', { title: 'Inicio de sesión' });
+};
+
+// Maneja el inicio de sesión
 exports.login = (req, res) => {
   const { email, password } = req.body;
-  console.log(email, password);
-    // Lógica de autenticación para login
-    res.send('Login exitoso');
-  };
-  
-  exports.register = async (req, res) => {
-    // Lógica de registro de usuarios
-    /* const name = req.body.name;
-    const lastname = req.body.lastname;
-    const age = req.body.age; */
-    const { username, age } = req.body;
-    res.send(`Registro exitoso ${username}`);
-    console.log(username)
-  };
+  console.log('Email:', email);
+  console.log('Password:', password);
+  // Lógica de autenticación para login
+  res.send('Login exitoso email: ' + email);
+};
 
-  exports.formRegistrar = (req, res) => {
-    res.render('auth/formRegistro',{title : "Registro"})
-  }
-  
+// Maneja el registro de usuarios
+exports.register = async (req, res) => {
+  const { username, age } = req.body;
+  console.log('Username:', username);
+  console.log('Age:', age);
+  // Aquí puedes agregar la lógica para guardar el usuario en la base de datos
+  res.send(`Registro exitoso ${username}`);
+};
+
+// Muestra el formulario de registro
+exports.formRegistrar = (req, res) => {
+  res.render('auth/formRegistro', { title: 'Registro' });
+};
