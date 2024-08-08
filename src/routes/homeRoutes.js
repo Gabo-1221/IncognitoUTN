ef499105853dc346de7d2811e65a2d7e16968e74
Osvaldo Villalba Mendoza <137123804+ValdoYKING@users.noreply.github.com>
2024-08-08 15:25:30 -0600

    add new dependency

--- src/routes/homeRoutes.js
+++ src/routes/homeRoutes.js
@@ -5,4 +5,8 @@ router.get('/', (req, res) => {
   res.render('home/home', { title: 'Incognito UTN' });
 });
 
+router.get('/como_funciona', (req, res) => {
+    res.render('home/como_funciona', { title: 'Incognito UTN | Como funciona' });
+  });
+
 module.exports = router;
