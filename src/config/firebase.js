05723bcbdbdbc1c59a40012687e153c53c3db5bc
ValdoYKING <osvaldovillalba-02@hotmail.com>
2024-08-22 15:05:14 -0600

    comment firebase

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,9 +4,6 @@ import { initializeApp } from "firebase/app";
 import { getStorage,ref } from 'firebase/storage';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
