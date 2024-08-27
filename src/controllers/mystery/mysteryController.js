e7b6d20be568febcb29c20063dc2c65ace732a77
valdoyk <osvaldovillalba-02@hotmail.com>
2024-08-27 14:12:34 -0600

    Vista para mystery shopper

--- src/controllers/mystery/mysteryController.js
+++ src/controllers/mystery/mysteryController.js
@@ -9,7 +9,9 @@ export const getHomeMystery = async (req, res) => {
     }
     const userData = await userHelper.getUserData(userId);
     if (userData) {
-      res.render('mystery/homeMystery', { title: 'Mystery', username: userData.username, rol: userData.rol });
+      res.render('mystery/homeMystery', { title: 'Mystery', username: userData.username, rol: userData.rol,
+          imagen: userData.imagen, activeSection: 'dashboard'
+       });
     } else {
       res.status(404).json({ message: 'Usuario no encontrado' });
     }
@@ -29,7 +31,9 @@ export const getPerfilMystery = async (req, res) => {
     if (userData) {
       res.render('perfil/perfilMystery', {
         title: 'Incognito UTN | Mi perfil', username: userData.username, rol: userData.rol,
-        apellido: userData.apellidos, email: userData.correo, fecha_nac: userData.fecha_nac, message: null, messageEmail: null, MessageNewPassword: null, MessageNewPasswordError: null,
+        apellido: userData.apellidos, email: userData.correo, fecha_nac: userData.fecha_nac,
+         message: null, messageEmail: null, MessageNewPassword: null, MessageNewPasswordError: null,
+         imagen: userData.imagen, activeSection: 'perfil'
       });
     } else {
       res.status(404).json({ message: 'Usuario no encontrado' });
